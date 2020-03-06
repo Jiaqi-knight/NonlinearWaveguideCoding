fbd2bfcfb3a979ad9bf63da60b5c9c3a2c177ed2
Jiaqi_Wang <Jiaqi_Wang@sjtu.edu.cn>
2020-03-06 14:57:39 +0800

    Create tensor4DiracFun.m

---
+++ coding2/tensor4DiracFun.m
@@ -0,0 +1,6 @@
+%-----------------------------------------------------------------%
+%this Code is to prove the tensor calculataion with Dirac's funcrion
+%\delta_{m,-n}\delta_{u,v}=[matrix]
+%provided by Jiaqi, email:Jiaqi_Wang@sjtu.edu.cn
+%2020-03-06
+%-----------------------------------------------------------------%
